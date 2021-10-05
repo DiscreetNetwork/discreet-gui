980d75a4787884044d1f4247180715734b9342c4
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 02:30:40 +0200

    MainNavigationStore implementation

---
+++ Discreet-GUI/WPF/Stores/MainNavigationStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF.ViewModels;
+
+namespace WPF.Stores
+{
+    /// <summary>
+    /// A Store to hold the ViewModelBase, that the MainWindow.xaml will be displaying
+    /// </summary>
+    public class MainNavigationStore
+    {
+        public event Action CurrentViewModelChanged;
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set { _currentViewModel = value; OnCurrentViewModelChanged(); }
+        }
+        void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
