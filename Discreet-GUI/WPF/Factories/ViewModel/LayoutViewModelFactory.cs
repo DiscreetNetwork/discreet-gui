e2bacb4294b8f68c953c1a27e1ed69905cc6aa62
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 11:30:10 +0200

    LayoutViewModel factory

---
+++ Discreet-GUI/WPF/Factories/ViewModel/LayoutViewModelFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF.ViewModels.Common;
+
+namespace WPF.Factories.ViewModel
+{
+    /// <summary>
+    /// A class that is responsible for creating and combining ViewModels based on a Type and a LayoutViewModel
+    /// </summary>
+    public class LayoutViewModelFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public LayoutViewModelFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ViewModelBase Create<TViewModel>() where TViewModel : ViewModelBase
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
