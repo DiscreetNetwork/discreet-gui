b82c2d76cd57aaacc26b4dd35407e42e7c06d372
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 03:02:25 +0200

    Added CurrentViewModel to MainWindowViewModel

--- Discreet-GUI/WPF/ViewModels/MainWindowViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WPF.Stores.Navigation;
 using WPF.ViewModels.Common;
 
 namespace WPF.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private readonly MainNavigationStore _mainNavigationStore;
+
+        public ViewModelBase CurrentViewModel => _mainNavigationStore.CurrentViewModel;
+
+        public MainWindowViewModel(MainNavigationStore mainNavigationStore)
+        {
+            _mainNavigationStore = mainNavigationStore;
+            _mainNavigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
     }
 }
