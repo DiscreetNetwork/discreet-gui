6ca993e47a8dc5a8103befb7d958ee3f9c5caebd
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 02:22:44 +0200

    Init

---
+++ Discreet-GUI/WPF/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF.ViewModels;
+using WPF.Views;
+
+namespace WPF
+{
+    public class Startup
+    {
+        private IHost _host;
+
+        public void Run (IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            _host = new HostBuilder().ConfigureServices((hostContext, services) =>
+            {
+
+            }).Build();
+
+            using IServiceScope serviceScope = _host.Services.CreateScope();
+
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(),
+            };
+        }
+    }
+}
