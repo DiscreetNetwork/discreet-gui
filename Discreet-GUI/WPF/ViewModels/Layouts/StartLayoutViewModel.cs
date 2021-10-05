1812be2bd58389d21b362fca7f3e21f3b1986d4c
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 14:41:03 +0200

    ViewModel for Start Layout

---
+++ Discreet-GUI/WPF/ViewModels/Layouts/StartLayoutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF.ViewModels.Common;
+
+namespace WPF.ViewModels.Layouts
+{
+    public class StartLayoutViewModel : ViewModelBase
+    {
+        public ViewModelBase ContentViewModel { get; set; }
+
+        public StartLayoutViewModel(ViewModelBase contentViewModel)
+        {
+            ContentViewModel = contentViewModel;
+        }
+    }
+}
