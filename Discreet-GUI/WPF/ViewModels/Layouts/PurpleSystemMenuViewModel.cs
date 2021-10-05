630624b17d1e9be041d87ed467c2e6809866e37e
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 12:57:13 +0200

    Initial viewModel for layout 'PurpleSystemMenuViewModel'

---
+++ Discreet-GUI/WPF/ViewModels/Layouts/PurpleSystemMenuViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF.ViewModels.Common;
+
+namespace WPF.ViewModels.Layouts
+{
+    public class PurpleSystemMenuViewModel : ViewModelBase
+    {
+        public ViewModelBase ContentViewModel { get; }
+
+        public PurpleSystemMenuViewModel(ViewModelBase contentViewModel)
+        {
+            ContentViewModel = contentViewModel;
+        }
+    }
+}
