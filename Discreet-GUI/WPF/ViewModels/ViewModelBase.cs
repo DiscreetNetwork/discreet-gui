f8814618d601ef0744f924b7fef8dcd1371d62c0
Ricki <ricki_ricko2@hotmail.com>
2021-10-05 02:25:32 +0200

    Add INotifyPropertyChanged implementation to ViewModelBase

--- Discreet-GUI/WPF/ViewModels/ViewModelBase.cs
+++ Discreet-GUI/WPF/ViewModels/ViewModelBase.cs
@@ -1,11 +1,17 @@
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace WPF.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase : ReactiveObject, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
