7118b95d0cdfa4d416a866bc3d3808f66f6a1a45
Ricki <ricki_ricko2@hotmail.com>
2021-10-07 09:05:04 +0200

    Wired MainWindowViewModel and MainWindow up with windowSettingsStore

--- Discreet-GUI/WPF/ViewModels/MainWindowViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
+using Avalonia.Controls;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using WPF.Stores;
 using WPF.Stores.Navigation;
 using WPF.ViewModels.Common;
 
@@ -8,19 +12,35 @@ namespace WPF.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private readonly WindowSettingsStore _windowSettingsStore;
         private readonly MainNavigationStore _mainNavigationStore;
 
+        public WindowState CurrentWindowState => _windowSettingsStore.CurrentWindowState;
         public ViewModelBase CurrentViewModel => _mainNavigationStore.CurrentViewModel;
 
-        public MainWindowViewModel(MainNavigationStore mainNavigationStore)
+        public MainWindowViewModel(WindowSettingsStore windowSettingsStore, MainNavigationStore mainNavigationStore)
         {
+            _windowSettingsStore = windowSettingsStore;
+            _windowSettingsStore.CurrentWindowStateChanged += OnCurrentWindowStateChanged;
             _mainNavigationStore = mainNavigationStore;
             _mainNavigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+
+            MaximizeWindowCommand = ReactiveCommand.Create(() =>
+            {
+                switch (WindowState)
+                {
+                    case WindowState.Maximized:
+                        WindowState = WindowState.Normal;
+                        break;
+
+                    case WindowState.Normal:
+                        WindowState = WindowState.Maximized;
+                        break;
+                }
+            });
         }
 
-        private void OnCurrentViewModelChanged()
-        {
-            OnPropertyChanged(nameof(CurrentViewModel));
-        }
+        private void OnCurrentWindowStateChanged() { OnPropertyChanged(nameof(CurrentWindowState)); }
+        private void OnCurrentViewModelChanged() { OnPropertyChanged(nameof(CurrentViewModel)); }
     }
 }
