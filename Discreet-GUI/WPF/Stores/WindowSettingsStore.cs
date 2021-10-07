cbde14ff6765f50924274bdc4fe7a868b24bdef2
Ricki <ricki_ricko2@hotmail.com>
2021-10-07 08:56:10 +0200

    Store class to hold MainWindow settings

---
+++ Discreet-GUI/WPF/Stores/WindowSettingsStore.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF.Stores
+{
+    public class WindowSettingsStore
+    {
+        public event Action CurrentWindowStateChanged;
+        private WindowState _currentWindowState = WindowState.Normal;
+        public WindowState CurrentWindowState
+        {
+            get => _currentWindowState;
+            private set { _currentWindowState = value; OnCurrentWindowStateChanged(); }
+        }
+        public void OnCurrentWindowStateChanged()
+        {
+            CurrentWindowStateChanged?.Invoke();
+        }
+    }
+}
