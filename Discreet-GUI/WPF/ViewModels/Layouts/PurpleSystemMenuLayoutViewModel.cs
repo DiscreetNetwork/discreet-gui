f658128d172f044f71e781166e76d73aeee191bf
Ricki <ricki_ricko2@hotmail.com>
2021-10-07 15:57:47 +0200

    Changed maximize from ReactiveCommand to method command

--- Discreet-GUI/WPF/ViewModels/Layouts/PurpleSystemMenuLayoutViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Layouts/PurpleSystemMenuLayoutViewModel.cs
@@ -1,9 +1,12 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reactive;
 using System.Text;
+using System.Windows.Input;
 using WPF.Stores;
 using WPF.ViewModels.Common;
 
@@ -12,27 +15,26 @@ namespace WPF.ViewModels.Layouts
     public class PurpleSystemMenuLayoutViewModel : ViewModelBase
     {
         private readonly WindowSettingsStore _windowSettingsStore;
-        public ReactiveCommand<Unit, Unit> ToggleWindowStateCommand { get; }
         public ViewModelBase ContentViewModel { get; }
 
         public PurpleSystemMenuLayoutViewModel(ViewModelBase contentViewModel, WindowSettingsStore windowSettingsStore)
         {
             ContentViewModel = contentViewModel;
             _windowSettingsStore = windowSettingsStore;
+        }
 
-            ToggleWindowStateCommand = ReactiveCommand.Create(() =>
+        public void ToggleWindowStateHandler()
+        {
+            switch (_windowSettingsStore.CurrentWindowState)
             {
-                switch (_windowSettingsStore.CurrentWindowState)
-                {
-                    case WindowState.Maximized:
-                        _windowSettingsStore.CurrentWindowState = WindowState.Normal;
-                        break;
+                case WindowState.Maximized:
+                    _windowSettingsStore.CurrentWindowState = WindowState.Normal;
+                    break;
 
-                    case WindowState.Normal:
-                        _windowSettingsStore.CurrentWindowState = WindowState.Maximized;
-                        break;
-                }
-            });
+                case WindowState.Normal:
+                    _windowSettingsStore.CurrentWindowState = WindowState.Maximized;
+                    break;
+            }
         }
     }
 }
