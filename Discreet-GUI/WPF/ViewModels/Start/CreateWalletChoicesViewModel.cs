4d3ddb8b6dfecee69eb5555262adbb3dbb55a3b9
Ricki <ricki_ricko2@hotmail.com>
2021-10-11 17:04:43 +0200

    Changes to recovery phrase view, and navigation from create wallet choices view

--- Discreet-GUI/WPF/ViewModels/Start/CreateWalletChoicesViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Start/CreateWalletChoicesViewModel.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WPF.Factories.Navigation;
+using WPF.Services.Navigation.Common;
 using WPF.ViewModels.Common;
 
 namespace WPF.ViewModels.Start
 {
     public class CreateWalletChoicesViewModel : ViewModelBase
     {
-        public CreateWalletChoicesViewModel()
+        INavigationService _navigateYourRecoveryPhraseService;
+        public CreateWalletChoicesViewModel(NavigationServiceFactory navigationServiceFactory)
         {
+            _navigateYourRecoveryPhraseService = navigationServiceFactory.Create<YourRecoveryPhraseViewModel>();
+        }
 
+        public void NavigateYourRecoveryPhraseView()
+        {
+            _navigateYourRecoveryPhraseService.Navigate();
         }
     }
 }
