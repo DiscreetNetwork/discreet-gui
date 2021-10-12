88cf0426211c759a5efb8c066870557d4b6efd89
Ricki <ricki_ricko2@hotmail.com>
2021-10-12 13:25:05 +0200

    Added fade to test notification for future reference

--- Discreet-GUI/WPF/ViewModels/Notifications/TestNotificationViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Notifications/TestNotificationViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
 using WPF.Factories.Navigation;
 using WPF.ViewModels.Common;
 
@@ -11,9 +12,16 @@ namespace WPF.ViewModels.Notifications
     class TestNotificationViewModel : ViewModelBase
     {
         public ReactiveCommand<Unit, Unit> CloseCommand { get; set; }
+
+        float _opacity = 1;
+        public float Opacity { get => _opacity; set { _opacity = value; OnPropertyChanged(nameof(Opacity)); } }
         public TestNotificationViewModel(NavigationServiceFactory navigationServiceFactory)
         {
-            CloseCommand = ReactiveCommand.Create(() => navigationServiceFactory.CreateModalNavigationService().Navigate());
+            CloseCommand = ReactiveCommand.Create(() =>
+            {
+                Opacity = 0;
+                Task.Delay(200).ContinueWith(_ => navigationServiceFactory.CreateModalNavigationService().Navigate());
+            });
         }
     }
 }
