fa03642a85e197e58ffb1eea9226115bf4b4740a
Ricki <ricki_ricko2@hotmail.com>
2021-10-13 14:50:49 +0200

    AccountLeftNavigation - Button active styling and hover active styling

--- Discreet-GUI/WPF/ViewModels/Layouts/Account/AccountLeftNavigationLayoutViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Layouts/Account/AccountLeftNavigationLayoutViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DynamicData.Binding;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive;
 using System.Text;
 using WPF.Stores.Navigation;
 using WPF.ViewModels.Common;
@@ -11,15 +15,36 @@ namespace WPF.ViewModels.Layouts.Account
         private readonly AccountNavigationStore _accountNavigationStore;
         public ViewModelBase CurrentViewModel => _accountNavigationStore.CurrentViewModel;
 
+        public ReactiveCommand<Unit, Unit> NavigateAccountHomeCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> NavigateAccountSendCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> NavigateAccountReceiveCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> NavigateAccountTransactionsCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> NavigateAccountSettingsCommand { get; set; }
+        public ObservableCollection<bool> ButtonActiveStates { get; set; } = new ObservableCollection<bool>() { true, false, false, false, false };
+
         public AccountLeftNavigationLayoutViewModel(AccountNavigationStore accountNavigationStore)
         {
             _accountNavigationStore = accountNavigationStore;
             accountNavigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+
+            NavigateAccountHomeCommand              = ReactiveCommand.Create(() => { ResetButtonStates(); ButtonActiveStates[0] = true; });
+            NavigateAccountSendCommand              = ReactiveCommand.Create(() => { ResetButtonStates(); ButtonActiveStates[1] = true; });
+            NavigateAccountReceiveCommand           = ReactiveCommand.Create(() => { ResetButtonStates(); ButtonActiveStates[2] = true; });
+            NavigateAccountTransactionsCommand      = ReactiveCommand.Create(() => { ResetButtonStates(); ButtonActiveStates[3] = true; });
+            NavigateAccountSettingsCommand          = ReactiveCommand.Create(() => { ResetButtonStates(); ButtonActiveStates[4] = true; });
         }
 
         private void OnCurrentViewModelChanged()
         {
             OnPropertyChanged(nameof(CurrentViewModel));
+        }
+
+        void ResetButtonStates()
+        {
+            for (int i = 0; i < ButtonActiveStates.Count; i++)
+            {
+                ButtonActiveStates[i] = false;
+            }
         }
     }
 }
