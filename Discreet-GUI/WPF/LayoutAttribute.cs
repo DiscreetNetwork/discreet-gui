1fd3332a2584a16b3cfdf564cddc533129364f44
Ricki <ricki_ricko2@hotmail.com>
2021-10-14 21:42:48 +0200

    LayoutAttribute.cs - Summary

--- Discreet-GUI/WPF/LayoutAttribute.cs
+++ Discreet-GUI/WPF/LayoutAttribute.cs
@@ -5,6 +5,10 @@ using WPF.ViewModels.Common;
 
 namespace WPF
 {
+    /// <summary>
+    /// Attribute to use on ViewModels, to specify what layouts to combine them with.       e.g  [Layout(typeof(TitleBarLayout), typeof(FillerLayout))]
+    /// First layout in the array, will be the entrypoint for the layout generation
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class LayoutAttribute : Attribute
     {
