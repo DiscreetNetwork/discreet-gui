9b3a3291b8411e2909117ed5fd3140c579f09b8b
Ricki <ricki_ricko2@hotmail.com>
2021-11-08 12:42:57 +0100

    QRCode generation method

---
+++ Discreet-GUI/WPF/ExtensionMethods/BitmapEx.cs
@@ -0,0 +1,34 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WPF.ExtensionMethods
+{
+    /// <summary>
+    /// Bitmap class to extend static methods
+    /// </summary>
+    public static class BitmapEx
+    {
+        /// <summary>
+        /// Creates a QRCode System.Drawing.Bitmap and converts it to a Avalonia.Media.Imaging.Bitmap to be used for Avalonia.Controls.Image controls
+        /// </summary>
+        /// <param name="text">The content to be encoded into the QR Code</param>
+        /// <returns>Avalonia specific Bitmap</returns>
+        public static Avalonia.Media.Imaging.Bitmap CreateQRCode(string text)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            System.Drawing.Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            using MemoryStream ms = new MemoryStream();
+            qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            ms.Position = 0;
+
+            Avalonia.Media.Imaging.Bitmap avaloniaBitmap = new Avalonia.Media.Imaging.Bitmap(ms);
+            return avaloniaBitmap;
+        }
+    }
+}
--- Discreet-GUI/WPF/ViewModels/Account/AccountReceiveViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Account/AccountReceiveViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using Avalonia.Threading;
+using QRCoder;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using WPF.ExtensionMethods;
 using WPF.ViewModels.Common;
 
 namespace WPF.ViewModels.Account
 {
     class AccountReceiveViewModel : ViewModelBase
     {
+        Avalonia.Media.Imaging.Bitmap _qrCode = null;
+        public Avalonia.Media.Imaging.Bitmap QrCode
+        {
+            get => _qrCode;
+            set
+            {
+                _qrCode = value;
+                OnPropertyChanged(nameof(QrCode));
+            }
+        }
 
+        public AccountReceiveViewModel()
+        {
+            QrCode = BitmapEx.CreateQRCode("0x57fuyw931209fj90wd");
+        }
     }
 }
