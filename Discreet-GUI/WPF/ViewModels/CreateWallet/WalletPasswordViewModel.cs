fd7d0d04a8126baaaab8c0dca59169d09783ebb1
Ricki <ricki_ricko2@hotmail.com>
2022-03-22 16:40:36 +0100

    Password functionality for showing and hiding the entered password

--- Discreet-GUI/WPF/ViewModels/CreateWallet/WalletPasswordViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/CreateWallet/WalletPasswordViewModel.cs
@@ -10,13 +10,45 @@ using WPF.ViewModels.Layouts;
 namespace WPF.ViewModels.CreateWallet
 {
     [Layout(typeof(DarkTitleBarLayoutWithBackButtonViewModel))]
-    class WalletPasswordViewModel : ViewModelBase
+    public class WalletPasswordViewModel : ViewModelBase
     {
         ReactiveCommand<Unit, Unit> NavigateWalletDetailsViewCommand { get; set; }
+
+
+        private string _enterPasswordCharacter = "*";
+        public string EnterPasswordCharacter { get => _enterPasswordCharacter; set { _enterPasswordCharacter = value; OnPropertyChanged(nameof(EnterPasswordCharacter)); } }
+
+        private bool _displayEnterPassword = false;
+        public bool DisplayEnterPassword { get => _displayEnterPassword; set { _displayEnterPassword = value; OnPropertyChanged(nameof(DisplayEnterPassword)); } }
+
+        ReactiveCommand<Unit, Unit> ToggleDisplayEnterPasswordCommand { get; set; }
+
+
+        private string _confirmPasswordCharacter = "*";
+        public string ConfirmPasswordCharacter { get => _confirmPasswordCharacter; set { _confirmPasswordCharacter = value; OnPropertyChanged(nameof(ConfirmPasswordCharacter)); } }
+
+        private bool _displayConfirmPassword = false;
+        public bool DisplayConfirmPassword { get => _displayConfirmPassword; set { _displayConfirmPassword = value; OnPropertyChanged(nameof(DisplayConfirmPassword)); } }
+
+        ReactiveCommand<Unit, Unit> ToggleDisplayConfirmPasswordCommand { get; set; }
+
+        public WalletPasswordViewModel() { }
 
         public WalletPasswordViewModel(NavigationServiceFactory navigationServiceFactory)
         {
             NavigateWalletDetailsViewCommand = ReactiveCommand.Create(navigationServiceFactory.CreateStackNavigation<WalletPasswordViewModel, WalletDetailsViewModel>().Navigate);
+
+            ToggleDisplayEnterPasswordCommand = ReactiveCommand.Create(() =>
+            {
+                DisplayEnterPassword = !DisplayEnterPassword;
+                EnterPasswordCharacter = DisplayEnterPassword ? string.Empty : "*";
+            });
+
+            ToggleDisplayConfirmPasswordCommand = ReactiveCommand.Create(() =>
+            {
+                DisplayConfirmPassword = !DisplayConfirmPassword;
+                ConfirmPasswordCharacter = DisplayConfirmPassword ? string.Empty : "*";
+            });
         }
     }
 }
--- Discreet-GUI/WPF/ViewModels/Modals/PasswordViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Modals/PasswordViewModel.cs
@@ -10,13 +10,27 @@ using WPF.ViewModels.Layouts;
 namespace WPF.ViewModels.Modals
 {
     [Layout(typeof(DarkTitleBarLayoutSimpleViewModel))]
-    class PasswordViewModel : ViewModelBase
+    public class PasswordViewModel : ViewModelBase
     {
         ReactiveCommand<Unit, Unit> EnterCommand { get; set; }
+
+
+        private bool _displayPassword = false;
+        public bool DisplayPassword { get => _displayPassword; set { _displayPassword = value; OnPropertyChanged(nameof(DisplayPassword)); } }
+        private string _passwordCharacter = "*";
+        public string PasswordCharacter { get => _passwordCharacter; set { _passwordCharacter = value; OnPropertyChanged(nameof(PasswordCharacter)); } }
+        ReactiveCommand<Unit, Unit> ToggleDisplayPasswordCommand { get; set; }
+
+        public PasswordViewModel() { }
 
         public PasswordViewModel(NavigationServiceFactory navigationServiceFactory)
         {
             EnterCommand = ReactiveCommand.Create(navigationServiceFactory.CreateModalNavigationService().Navigate);
+            ToggleDisplayPasswordCommand = ReactiveCommand.Create(() =>
+            {
+                DisplayPassword = !DisplayPassword;
+                PasswordCharacter = DisplayPassword ? string.Empty : "*";
+            });
         }
     }
 }
