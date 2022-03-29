975c01851c2667dd598e9f2e5a79de822235288e
Ricki <ricki_ricko2@hotmail.com>
2022-03-29 20:38:54 +0200

    Input validation for WalletDetails view

--- Discreet-GUI/WPF/ViewModels/CreateWallet/WalletDetailsViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/CreateWallet/WalletDetailsViewModel.cs
@@ -23,15 +23,40 @@ namespace WPF.ViewModels.CreateWallet
         ReactiveCommand<Unit, Unit> NavigateWalletCreatedViewCommand { get; set; }
         public ReactiveCommand<Unit, Unit> NavigateBackCommand { get; set; }
 
-        public string WalletName { get => _newWalletCache.WalletName; set => _newWalletCache.WalletName = value; }
-        public string WalletLocation { get => _newWalletCache.WalletLocation; set => _newWalletCache.WalletLocation = value; }
+        public string WalletName { get => _newWalletCache.WalletName; set { _newWalletCache.WalletName = value; ValidateCanContinue(); } }
+        public string WalletLocation { get => _newWalletCache.WalletLocation; set { _newWalletCache.WalletLocation = value; ValidateCanContinue(); } }
         public bool Bootstrap { get => _newWalletCache.Bootstrap; set => _newWalletCache.Bootstrap = value; }
+
+
+        private bool _canContinue = false;
+        public bool CanContinue { get => _canContinue; set { _canContinue = value; OnPropertyChanged(nameof(CanContinue)); } }
+
 
         public WalletDetailsViewModel(NavigationServiceFactory navigationServiceFactory, NewWalletCache newWalletCache)
         {
             NavigateWalletCreatedViewCommand = ReactiveCommand.Create(navigationServiceFactory.Create<WalletCreatedSuccessfullyViewModel>().Navigate);
             NavigateBackCommand = ReactiveCommand.Create(navigationServiceFactory.Create<WalletPasswordViewModel>().Navigate);
             _newWalletCache = newWalletCache;
+
+            ValidateCanContinue();
+        }
+
+
+        public void ValidateCanContinue()
+        {
+            if(string.IsNullOrWhiteSpace(WalletName))
+            {
+                CanContinue = false;
+                return;
+            }
+
+            if(string.IsNullOrWhiteSpace(WalletLocation))
+            {
+                CanContinue = false;
+                return;
+            }
+
+            CanContinue = true;
         }
     }
 }
