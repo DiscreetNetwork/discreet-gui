7936db5590fc8f67294503c6f405052472b556ab
Ricki <ricki_ricko2@hotmail.com>
2022-03-30 17:42:06 +0200

    Changed scrollViewer tables to auto adjust their height, based on screen height

--- Discreet-GUI/WPF/ViewModels/Account/AccountHomeViewModel.cs
+++ Discreet-GUI/WPF/ViewModels/Account/AccountHomeViewModel.cs
@@ -8,27 +8,25 @@ namespace WPF.ViewModels.Account
 {
     public class AccountHomeViewModel : ViewModelBase
     {
-        public ObservableCollection<MockItem> MockItems { get; set; } = new ObservableCollection<MockItem>
-        {
-            new MockItem
-            {
-               AccountLabel = "First account name",
-               AccountBalance = 1127.50f
-            },
-            new MockItem
-            {
-               AccountLabel = "Second account name",
-               AccountBalance = 7751.10f
-            },
-        };
+        public ObservableCollection<MockItem> MockItems { get; set; } = new ObservableCollection<MockItem>(MockItem.Generate(24));
+
     }
-
-
 
 
     public class MockItem
     {
         public string AccountLabel { get; set; }
         public float AccountBalance { get; set; }
+
+        public static List<MockItem> Generate(int amount)
+        {
+            List<MockItem> items = new List<MockItem>();
+            for (int i = 0; i < amount; i++)
+            {
+                items.Add(new MockItem() { AccountLabel = "Account name", AccountBalance = 7777.77f });
+            }
+
+            return items;
+        }
     }
 }
