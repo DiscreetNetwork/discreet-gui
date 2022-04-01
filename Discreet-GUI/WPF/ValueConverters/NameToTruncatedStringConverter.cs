83152ec76280c6ddd9128d10c13eb3a503ec0307
Ricki <ricki_ricko2@hotmail.com>
2022-04-01 03:57:58 +0200

    Truncated name on home screen - removed delay from Send transaction confirm window transition

---
+++ Discreet-GUI/WPF/ValueConverters/NameToTruncatedStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.ValueConverters
+{
+    public class NameToTruncatedStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = (string)value;
+            return $"{s.Substring(0, s.Length < 10 ? s.Length : 10)}...";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
