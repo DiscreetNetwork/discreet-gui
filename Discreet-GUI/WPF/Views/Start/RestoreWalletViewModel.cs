f6ad2952940801ca07e6990e58cb1f89c60de609
Ricki <ricki_ricko2@hotmail.com>
2022-04-21 03:40:48 +0200

    Design for recover wallet view

--- Discreet-GUI/WPF/Views/Start/RestoreWalletViewModel.cs
+++ Discreet-GUI/WPF/Views/Start/RestoreWalletViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
 using WPF.Factories.Navigation;
 using WPF.ViewModels.Common;
 using WPF.Views.Account;
@@ -17,36 +18,50 @@ namespace WPF.Views.Start
     [Layout(typeof(DarkTitleBarLayoutWithBackButtonViewModel))]
     class RestoreWalletViewModel : ViewModelBase
     {
-        public ReactiveCommand<Unit, Unit> NextCommand { get; set; }
-        public ReactiveCommand<Unit, Unit> BackCommand { get; set; }
-        public ReactiveCommand<Unit, Unit> OpenFileDialogCommand { get; set; }
+        private readonly NavigationServiceFactory _navigationServiceFactory;
+
+        public string WalletName { get; set; }
 
         private string _selectedFilePath = string.Empty;
         public string SelectedFilePath { get => _selectedFilePath; set { _selectedFilePath = value; OnPropertyChanged(nameof(SelectedFilePath)); } }
 
+        // RadioButtons
+        private bool _fromMnemonicSeed = true;
+        public bool FromMnemonicSeed { get => _fromMnemonicSeed; set { _fromMnemonicSeed = value; OnPropertyChanged(nameof(FromMnemonicSeed)); } }
+
+        private bool _fromKeys = false;
+        public bool FromKeys { get => _fromKeys; set { _fromKeys = value; OnPropertyChanged(nameof(FromKeys)); } }
+
+        public string MnemonicPhrase { get; set; }
+
         public RestoreWalletViewModel(NavigationServiceFactory navigationServiceFactory)
         {
-            NextCommand = ReactiveCommand.Create(() =>
+            _navigationServiceFactory = navigationServiceFactory;
+        }
+
+        void NextCommand()
+        {
+            _navigationServiceFactory.CreateAccountNavigation<AccountLeftNavigationLayoutViewModel>().Navigate();
+            _navigationServiceFactory.CreateAccountNavigation<AccountHomeViewModel>().Navigate();
+        }
+
+        void BackCommand()
+        {
+            _navigationServiceFactory.Create<ExistingWalletChoicesViewModel>().Navigate();
+        }
+
+        async Task OpenFileDialogCommand()
+        {
+            var dialog = new OpenFolderDialog();
+
+            if (Avalonia.Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                navigationServiceFactory.CreateAccountNavigation<AccountLeftNavigationLayoutViewModel>().Navigate();
-                navigationServiceFactory.CreateAccountNavigation<AccountHomeViewModel>().Navigate();
-            });
-
-            BackCommand = ReactiveCommand.Create(navigationServiceFactory.Create<ExistingWalletChoicesViewModel>().Navigate);
-
-            OpenFileDialogCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                var dialog = new OpenFolderDialog();
-
-                if (Avalonia.Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                var result = await dialog.ShowAsync(desktop.MainWindow);
+                if (result != null)
                 {
-                    var result = await dialog.ShowAsync(desktop.MainWindow);
-                    if(result != null)
-                    {
-                        SelectedFilePath = result;
-                    }
+                    SelectedFilePath = result;
                 }
-            });
+            }
         }
     }
 }
