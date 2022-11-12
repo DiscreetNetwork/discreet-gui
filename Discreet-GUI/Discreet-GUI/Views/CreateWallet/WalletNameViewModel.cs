3c0f83c7793d9e272c22293d8722b43b778bb198
Ricki <ricki_ricko2@hotmail.com>
2022-11-12 16:37:01 +0100

    Check for duplicate wallet in 'WalletNameView'

--- Discreet-GUI/Discreet-GUI/Views/CreateWallet/WalletNameViewModel.cs
+++ Discreet-GUI/Discreet-GUI/Views/CreateWallet/WalletNameViewModel.cs
@@ -8,17 +8,18 @@ using Discreet_GUI.ViewModels.Common;
 using Discreet_GUI.Views.Layouts;
 using Discreet_GUI.Views.Start;
 using Services.Daemon.Wallet;
+using System.Threading.Tasks;
+using Discreet_GUI.Services;
 
 namespace Discreet_GUI.Views.CreateWallet
 {
     [Layout(typeof(DarkTitleBarLayoutWithBackButtonViewModel))]
     class WalletNameViewModel : ViewModelBase
     {
+        private readonly NavigationServiceFactory _navigationServiceFactory;
         private readonly NewWalletCache _newWalletCache;
         private readonly DaemonWalletService _walletService;
-
-        public ReactiveCommand<Unit, Unit> NavigateYourRecoveryPhraseViewCommand { get; set; }
-        public ReactiveCommand<Unit, Unit> NavigateBackCommand { get; set; }
+        private readonly NotificationService _notificationService;
 
         public string WalletName { get => _newWalletCache.WalletName; set { _newWalletCache.WalletName = value; ValidateCanContinue(); } }
 
@@ -26,16 +27,15 @@ namespace Discreet_GUI.Views.CreateWallet
         private bool _canContinue = false;
         public bool CanContinue { get => _canContinue; set { _canContinue = value; OnPropertyChanged(nameof(CanContinue)); } }
 
-        private bool _isLoading;
+        private bool _isLoading = true;
         public bool IsLoading { get => _isLoading; set { _isLoading = value; OnPropertyChanged(nameof(IsLoading)); } }
 
-        public WalletNameViewModel(NavigationServiceFactory navigationServiceFactory, NewWalletCache newWalletCache, DaemonWalletService walletService)
+        public WalletNameViewModel(NavigationServiceFactory navigationServiceFactory, NewWalletCache newWalletCache, DaemonWalletService walletService, NotificationService notificationService)
         {
+            _navigationServiceFactory = navigationServiceFactory;
             _newWalletCache = newWalletCache;
             _walletService = walletService;
-            NavigateYourRecoveryPhraseViewCommand = ReactiveCommand.Create(navigationServiceFactory.Create<YourRecoveryPhraseViewModel>().Navigate);
-            NavigateBackCommand                     = ReactiveCommand.Create(navigationServiceFactory.Create<StartViewModel>().Navigate);
-
+            _notificationService = notificationService;
             ValidateCanContinue();
             RxApp.MainThreadScheduler.Schedule(OnActivated);
         }
@@ -48,6 +48,13 @@ namespace Discreet_GUI.Views.CreateWallet
             if (_newWalletCache.Mnemonic is null)
             {
                 var mnemonic = await _walletService.GetMnemonic();
+                if(mnemonic is null)
+                {
+                    IsLoading = false;
+                    _notificationService.DisplayError("An error occured while trying to generate the mnemonic.");
+                    return;
+                }
+
                 _newWalletCache.Mnemonic = mnemonic.Value.Split(' ').Select(x => x).ToList();
             }
 
@@ -69,6 +76,35 @@ namespace Discreet_GUI.Views.CreateWallet
             }
 
             CanContinue = true;
+        }
+
+
+        async Task NavigateYourRecoveryPhraseViewCommand()
+        {
+            IsLoading = true;
+
+            var wallets = await _walletService.GetWallets();
+            if(wallets is null)
+            {
+                _notificationService.DisplayError("An error occured while trying to fetch existing wallets.");
+                IsLoading = false;
+                return;
+            }
+
+            if(wallets.Any(w => w.Label == WalletName))
+            {
+                _notificationService.DisplayError("A wallet with the specified label already exist.");
+                IsLoading = false;
+                return;
+            }
+
+            IsLoading = false;
+            _navigationServiceFactory.Create<YourRecoveryPhraseViewModel>().Navigate();
+        }
+
+        void NavigateBackCommand()
+        {
+            _navigationServiceFactory.Create<StartViewModel>().Navigate();
         }
     }
 }
