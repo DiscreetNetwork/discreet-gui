22f9ec7c225395dc358da0ff851590d3705a7df2
Ricki <ricki_ricko2@hotmail.com>
2022-11-17 16:22:16 +0100

    Design changes + zmq-connections-changed

---
+++ Discreet-GUI/Services/ZMQ/Handlers/ConnectionsChangedHandler.cs
@@ -0,0 +1,30 @@
+﻿using Services.Caches;
+using Services.Daemon.Status;
+using Services.ZMQ.Handlers.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ZMQ.Handlers
+{
+    public class ConnectionsChangedHandler : MessageHandler
+    {
+        private readonly DaemonStatusService _daemonStatusService;
+        private readonly WalletCache _walletCache;
+
+        public ConnectionsChangedHandler(DaemonStatusService daemonStatusService, WalletCache walletCache)
+        {
+            _daemonStatusService = daemonStatusService;
+            _walletCache = walletCache;
+        }
+
+        public override Task Handle(byte[] bytes)
+        {
+            var numConnections = BitConverter.ToInt32(bytes);
+            _walletCache.NumberOfConnections = numConnections;
+            return Task.CompletedTask;
+        }
+    }
+}
